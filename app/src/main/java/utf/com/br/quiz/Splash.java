2a53464d3001a3a4dbb9fd4fdf58b5f90defbbe0
Kaliane <kally_lari@hotmail.com>
2019-05-19 16:17:41 -0300

    Initial commit

---
+++ app/src/main/java/utf/com/br/quiz/Splash.java
@@ -0,0 +1,29 @@
+package utf.com.br.quiz;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //Gera um delai de 5 segundos
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
+    }
+}
